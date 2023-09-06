4eb4096131ca050567d623bb947c7073dc6dd75e
Kinza Akbar R <akbarrx126@gmail.com>
2023-09-06 14:32:57 +0700

    create user service

---
+++ src/test/java/com/smk/cashier/service/UserServiceTest.java
@@ -0,0 +1,30 @@
+package com.smk.cashier.service;
+
+import com.smk.cashier.model.User;
+import org.junit.jupiter.api.Order;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @org.junit.jupiter.api.Test
+    @Order(2)
+    void getUserList() {
+        List<User> userList =
+                UserService
+                        .getInstance().getUserList();
+        assertEquals(userList.size(),1);
+    }
+
+    @org.junit.jupiter.api.Test
+    @Order(1)
+    void addUser() {
+        User user = new User();
+        user.setUsername("BinaInformatika");
+        user.setPassword(("KarnaSan"));
+        UserService
+                .getInstance().addUser(user);
+    }
+}
