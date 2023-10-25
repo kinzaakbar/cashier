f62b406c83e3757d99286babb4eed2943abd33ff
Kinza Akbar R <akbarrx126@gmail.com>
2023-10-25 11:32:09 +0700

    Adding BarangDao get method and StockDao

---
+++ src/main/java/com/smk/cashier/dao/StokDao.java
@@ -0,0 +1,87 @@
+package com.smk.cashier.dao;
+
+import com.smk.cashier.model.Stok;
+
+import java.sql.*;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+
+public class StokDao implements Dao<Stok, Integer>{
+    private final Optional<Connection> connection;
+
+    public StokDao() {
+        connection = JdbcConnection.getConnection();
+    }
+
+    @Override
+    public Optional<Stok> get(int id) {
+        return connection.flatMap(conn -> {
+            Optional<Stok> stok = Optional.empty();
+            String sql = "SELECT * from stok where stok_id = ?";
+            try {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setInt(1, id);
+                ResultSet rs = ps.executeQuery();
+                if(rs.next()){
+                    int idStok = rs.getInt("stok_id");
+                    String kodeBarang = rs.getString("kode_barang");
+                    int stokBarang = rs.getInt("stok_barang");
+
+                    Stok stokResult = new Stok();
+                    stokResult.setId(idStok);
+                    stokResult.setKodeBarang(kodeBarang);
+                    stokResult.setStokBarang(stokBarang);
+
+                    stok = Optional.of(stokResult);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return stok;
+        });
+    }
+
+    @Override
+    public Collection<Stok> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Integer> save(Stok stok) {
+        Stok nonNullBarang = Objects.requireNonNull(stok);
+        String sql = "INSERT INTO Barang(kode_barang, stok_barang, created_by, updated_by, date_created, last_modified) VALUES(?,?,?,?,?,?)";
+        return connection.flatMap(conn -> {
+            Optional<Integer> generatedId = Optional.empty();
+            try {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setString(1, stok.getKodeBarang());
+                ps.setInt(2, stok.getStokBarang());
+                ps.setString(3, stok.getCreatedBy());
+                ps.setString(4, stok.getUpdatedBy());
+                ps.setDate(5, new Date(stok.getDateCreated().getTime()));
+                ps.setDate(6, new Date(stok.getLastModified().getTime()));
+                int numberOfInsertRows = ps.executeUpdate();
+                if (numberOfInsertRows > 0){
+                    ResultSet rs = ps.getGeneratedKeys();
+                    if (rs.next()){
+                        generatedId = Optional.of(rs.getInt(1));
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return generatedId;
+        });
+    }
+
+    @Override
+    public void update(Stok stok) {
+
+    }
+
+    @Override
+    public void delete(Stok stok) {
+
+    }
+}
--- src/test/java/com/smk/cashier/service/StokServiceTest.java
+++ src/test/java/com/smk/cashier/service/StokServiceTest.java
@@ -1,17 +1,24 @@
 package com.smk.cashier.service;
 
+import com.smk.cashier.dao.StokDao;
 import com.smk.cashier.model.Stok;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StokServiceTest {
+
+
+
 
     @Test
     @Order(2)
@@ -48,5 +55,61 @@ class StokServiceTest {
         laptop.setStokBarang(3);
         StokService.getInstance().addStok(laptopGaming);
 
+    }
+    @Test
+    @Order(4)
+    void saveStokDatabase(){
+        StokDao stokDao = new StokDao();
+        Stok laptop = new Stok();
+        laptop.setKodeBarang("LP001");
+        laptop.setStokBarang(20);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        stokDao.save(laptop);
+
+        Stok mouse = new Stok();
+        mouse.setKodeBarang("M0001");
+        mouse.setStokBarang(100);
+        mouse.setDateCreated(new Date());
+        mouse.setLastModified(new Date());
+        stokDao.save(mouse);
+
+        Stok laptopGaming = new Stok();
+        laptopGaming.setKodeBarang("LP001");
+        laptopGaming.setStokBarang(15);
+        laptopGaming.setDateCreated(new Date());
+        laptopGaming.setLastModified(new Date());
+        stokDao.save(laptopGaming);
+    }
+    @Test
+    @Order(5)
+    void getStokDatabase(){
+        StokDao stokDao = new StokDao();
+        Optional<Stok> stok1 = stokDao.get(1);
+        stok1.ifPresent(new Consumer<Stok>() {
+            @Override
+            public void accept(Stok stok) {
+                assertEquals(stok.getKodeBarang(), "LP001");
+                assertEquals(stok.getStokBarang(), 20);
+            }
+        });
+
+        Optional<Stok> stok2 = stokDao.get(2);
+        stok2.ifPresent(new Consumer<Stok>() {
+            @Override
+            public void accept(Stok stok) {
+                assertEquals(stok.getKodeBarang(), "M0001");
+                assertEquals(stok.getStokBarang(), 100);
+            }
+        });
+
+        Optional<Stok> stok3 = stokDao.get(3);
+        stok3.ifPresent(new Consumer<Stok>() {
+            @Override
+            public void accept(Stok stok) {
+                assertEquals(stok.getKodeBarang(), "LP002");
+                assertEquals(stok.getStokBarang(), 15);
+            }
+        });
     }
 }
