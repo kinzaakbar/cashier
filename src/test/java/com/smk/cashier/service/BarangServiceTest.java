38676701e1bb2777c2e63acb14f0a40ba9bc8c47
KarnaSan <akbarrx126@gmail.com>
2023-11-22 13:13:46 +0700

    Create barang delete and search

--- src/test/java/com/smk/cashier/service/BarangServiceTest.java
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -1,6 +1,7 @@
 package com.smk.cashier.service;
 
-import com.smk.cashier.Dao.BarangDao;
+import com.smk.cashier.dao.BarangDao;
+import com.smk.cashier.dao.BarangDao;
 import com.smk.cashier.model.Barang;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -134,4 +135,51 @@ class BarangServiceTest {
                 }
             });
         }
+    @Test
+    @Order(7)
+    void deleteBarang(){
+        BarangDao barangDao = new BarangDao();
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        barangDao.delete(laptop);
+
+        Optional<Barang> barang1 = barangDao.get(1);
+        if (barang1.isEmpty()){
+            assertEquals(barang1.isPresent(), false);
+        }
+    }
+
+    @Test
+    @Order(8)
+    void searchBarang(){
+        BarangDao barangDao = new BarangDao();
+        Barang wirelessKeyboard = new Barang();
+        wirelessKeyboard.setKodeBarang("WI001");
+        wirelessKeyboard.setNamaBarang("Wireless Keyboard");
+        wirelessKeyboard.setHargaBarang(1500000);
+        wirelessKeyboard.setDateCreated(new Date());
+        wirelessKeyboard.setLastModified(new Date());
+        barangDao.save(wirelessKeyboard);
+
+        Barang wirelessMouse = new Barang();
+        wirelessMouse.setKodeBarang("WI002");
+        wirelessMouse.setNamaBarang("Wireless Mouse");
+        wirelessMouse.setHargaBarang(900000);
+        wirelessMouse.setDateCreated(new Date());
+        wirelessMouse.setLastModified(new Date());
+        barangDao.save(wirelessMouse);
+
+        Barang mechanicalKeyboard = new Barang();
+        mechanicalKeyboard.setKodeBarang("ME001");
+        mechanicalKeyboard.setNamaBarang("Mechanical Keyboard");
+        mechanicalKeyboard.setHargaBarang(3000000);
+        mechanicalKeyboard.setDateCreated(new Date());
+        mechanicalKeyboard.setLastModified(new Date());
+        barangDao.save(mechanicalKeyboard);
+
+        assertEquals(barangDao.search("Mecha").size(),1);
+        assertEquals(barangDao.search("Key").size(),2);
+        assertEquals(barangDao.search("WI").size(),2);
+
+    }
     }
